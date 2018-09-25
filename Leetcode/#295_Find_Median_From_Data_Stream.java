a23612b75f79ef57ea977e41b70fd8258879e1cb
mintycc <mintyck@gmail.com>
2018-09-25 16:42:36 -0400

    Solution to problem 295 added

--- Leetcode/#295_Find_Median_From_Data_Stream.java
+++ Leetcode/#295_Find_Median_From_Data_Stream.java
@@ -46,3 +46,44 @@ class MedianFinder {
  * obj.addNum(num);
  * double param_2 = obj.findMedian();
  */
+
+class MedianFinder {
+
+    PriorityQueue<Integer> left;
+    PriorityQueue<Integer> right;
+    int size, ls, rs;
+    double median;
+    
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        left = new PriorityQueue<Integer>(Comparator.reverseOrder());
+        right = new PriorityQueue<Integer>();
+        size = ls = rs = 0;
+    }
+    
+    public void addNum(int num) {
+        size ++;
+        ls = (size + 1) / 2;
+        rs = size / 2;
+        if (left.isEmpty() || num < median)
+            left.add(num);
+        else right.add(num);
+        if (left.size() > ls)
+            right.add(left.poll());
+        if (right.size() > rs)
+            left.add(right.poll());
+        if (ls > rs) median = left.peek();
+        else median = (double)(left.peek() + right.peek()) / 2;
+    }
+    
+    public double findMedian() {
+        return median;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
