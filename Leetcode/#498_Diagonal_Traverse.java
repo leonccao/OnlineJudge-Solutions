9af75436848a30bc6b3a455de3137d4d6cff4045
mintycc <mintyck@gmail.com>
2018-09-25 11:34:35 -0400

    Solution to problem 498 updated

--- Leetcode/#498_Diagonal_Traverse.java
+++ Leetcode/#498_Diagonal_Traverse.java
@@ -33,3 +33,28 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int[] findDiagonalOrder(int[][] matrix) {
+        if (matrix.length == 0)
+            return new int[0];
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int[] ans = new int[m * n];
+        int i = 0, j = 0;
+        for (int idx = 0; idx < ans.length; idx ++) {
+            ans[idx] = matrix[i][j];
+            if ((i + j) % 2 == 0) { // go upwards
+                if (j == n - 1) i ++;
+                else if (i == 0) j ++;
+                else { i --; j ++; }
+            } else { // go downwards
+                if (i == m - 1) j ++;
+                else if (j == 0) i ++;
+                else {i ++; j --; }
+            }
+        }
+        return ans;
+    }
+}
