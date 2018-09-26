497ce9172cf319db5d7ebb5ef5cca915a05cd9e1
mintycc <mintyck@gmail.com>
2018-09-26 00:34:45 -0400

    Solution to problem 215 updated

--- Leetcode/#215_Kth_Largest_Element_in_an_Array.java
+++ Leetcode/#215_Kth_Largest_Element_in_an_Array.java
@@ -1,33 +1,28 @@
-/*
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length - k];
-    }
-}
-*/
-
-class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        return findKth(nums, 0, nums.length - 1, k - 1);
-    }
-    
-    private int findKth(int[] nums, int s, int e, int k) {
-        if (s == e) return nums[s];
-        int mid = nums[e];
-        int l, r;
-        for (l = s, r = e; l < r; l ++) {
-            if (nums[l] < mid) swap(nums, l --, -- r);
-        }
-        swap(nums, l, e);
-        if (l == k) return nums[l];
-        if (k < l) return findKth(nums, s, l - 1, k);
-        else return findKth(nums, l + 1, e, k);
-    }
     
     private void swap(int[] nums, int a, int b) {
         int tmp = nums[a];
         nums[a] = nums[b];
         nums[b] = tmp;
+    }
+    
+    private int findkth(int[] nums, int l, int r, int k) {
+        if (l == r) return nums[l];
+        int pivot = nums[r];
+        int i = l, j = r - 1;
+        do {
+            while (i < r && nums[i] > pivot) i ++;
+            while (l < j && nums[j] < pivot) j --;
+            if (i < j) swap(nums, i ++, j --);
+        } while (i < j);
+        swap(nums, i, r);
+        
+        if (i == k) return nums[i];
+        if (k < i) return findkth(nums, l, i - 1, k);
+        else return findkth(nums, i + 1, r, k);
+    }
+    
+    public int findKthLargest(int[] nums, int k) {
+        return findkth(nums, 0, nums.length - 1, k - 1);
     }
 }
