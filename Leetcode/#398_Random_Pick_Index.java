c4a9f2bde3d5dc81a6675975a95662e6b70c413c
mintycc <mintyck@gmail.com>
2018-09-27 10:25:30 -0400

    Solution to problem 398 updated

--- Leetcode/#398_Random_Pick_Index.java
+++ Leetcode/#398_Random_Pick_Index.java
@@ -52,3 +52,30 @@ class Solution {
  * Solution obj = new Solution(nums);
  * int param_1 = obj.pick(target);
  */
+
+class Solution {
+
+    Random random;
+    int[] nums;
+    
+    public Solution(int[] nums) {
+        this.nums = nums;
+        random = new Random();
+    }
+    
+    public int pick(int target) {
+        int rtn = -1, count = 0;
+        for (int i = 0; i < nums.length; i ++){
+            if (nums[i] != target) continue;
+            if (random.nextInt(++ count) == 0)
+                rtn = i;
+        }
+        return rtn;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
