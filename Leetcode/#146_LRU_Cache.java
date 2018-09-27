1e3f04e565de4c92c517dcd368407736b7dc1fa6
mintycc <mintyck@gmail.com>
2018-09-26 23:28:44 -0400

    Solution to problem 146 updated

--- Leetcode/#146_LRU_Cache.java
+++ Leetcode/#146_LRU_Cache.java
@@ -60,3 +60,76 @@ class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
+
+class LRUCache {
+
+    class DoublyLinkedList {
+        int key, val;
+        DoublyLinkedList prev, next;
+        DoublyLinkedList(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+    
+    Map<Integer, DoublyLinkedList> map;
+    int limit;
+    DoublyLinkedList head;
+    DoublyLinkedList tail;
+    
+    private void deleteDLL(DoublyLinkedList node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    
+    private void insertDLL(DoublyLinkedList node) {
+        node.prev = tail.prev;
+        node.next = tail;
+        node.prev.next = node;
+        node.next.prev = node;
+    }
+    
+    public LRUCache(int capacity) {
+        limit = capacity;
+        map = new HashMap<Integer, DoublyLinkedList>();
+        head = new DoublyLinkedList(0, 0);
+        tail = new DoublyLinkedList(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+        DoublyLinkedList tmp = map.get(key);
+        deleteDLL(tmp);
+        insertDLL(tmp);
+        return tmp.val;
+    }
+    
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            DoublyLinkedList tmp = map.get(key);
+            tmp.val = value;
+            deleteDLL(tmp);
+            insertDLL(tmp);
+            return;
+        }
+        
+        DoublyLinkedList tmp = new DoublyLinkedList(key, value);
+        map.put(key, tmp);
+        insertDLL(tmp);
+        if (map.size() > limit) {
+            int rm = head.next.key;
+            deleteDLL(head.next);
+            map.remove(rm);
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
