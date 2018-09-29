a84c46a1afb89ce11b3dd1c01585326e28423734
mintycc <mintyck@gmail.com>
2018-09-29 17:30:48 -0400

    Solution to problem 056 updated

---
+++ Leetcode/#056_Merge_Intervals.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        Collections.sort(intervals, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval a, Interval b) {
+                return a.start - b.start;
+            }
+        });
+        
+        List<Interval> ans = new ArrayList<Interval>();
+        Interval cur = null;
+        for (Interval interval : intervals) {
+            if (cur == null) {
+                cur = interval;
+                continue;
+            }
+            if (interval.start > cur.end) {
+                ans.add(cur);
+                cur = interval;
+            } else {
+                cur.end = Math.max(cur.end, interval.end);
+            }         
+        }
+        if (cur != null) ans.add(cur);
+        return ans;
+    }
+}
