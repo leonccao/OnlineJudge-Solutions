756d9c106803a39b16ea4fc54f18df82cccbc06b
mintycc <mintyck@gmail.com>
2018-09-29 15:46:12 -0400

    Solution to problem 091 updated

--- Leetcode/#091_Decode_Ways.java
+++ Leetcode/#091_Decode_Ways.java
@@ -23,3 +23,24 @@ class Solution {
         return f[s.length() - 1];
     }
 }
+
+/*
+Bugs
+1. String.length()
+*/
+class Solution {
+    public int numDecodings(String s) {
+        int[] f = new int[s.length() + 1];
+        f[0] = 1;
+        for (int i = 1; i <= s.length(); i ++) {
+            int tmp = s.charAt(i - 1) - '0';
+            if (tmp > 0) f[i] += f[i - 1];
+            if (i > 1) {
+                tmp = (s.charAt(i - 2) - '0') * 10 + s.charAt(i - 1) - '0';
+                if (tmp > 9 && tmp <= 26)
+                    f[i] += f[i - 2];
+            }
+        }
+        return f[s.length()];
+    }
+}
