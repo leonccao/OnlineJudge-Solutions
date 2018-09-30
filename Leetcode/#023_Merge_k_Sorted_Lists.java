50d6735435567f683e28146b7474a4d8c48afb31
mintycc <mintyck@gmail.com>
2018-09-30 16:02:22 -0400

    Solution to problem 023 updated

---
+++ Leetcode/#023_Merge_k_Sorted_Lists.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(
+            new Comparator<ListNode>() {
+                @Override
+                public int compare(ListNode a, ListNode b) {
+                    return a.val - b.val;
+                }
+            }
+        );
+        for (ListNode list : lists) 
+            if (list != null)
+                pq.add(list);
+        
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while (!pq.isEmpty()) {
+            tail.next = pq.poll();
+            tail = tail.next;
+            if (tail.next != null)
+                pq.add(tail.next);
+        }
+        return head.next;
+    }
+}
