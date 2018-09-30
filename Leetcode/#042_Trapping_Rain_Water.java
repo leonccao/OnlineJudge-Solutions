73c738341ce9fa9ca63502d2b0ade1404b3d6043
mintycc <mintyck@gmail.com>
2018-09-30 18:11:10 -0400

    Solution to problem 042 updated

--- Leetcode/#042_Trapping_Rain_Water.java
+++ Leetcode/#042_Trapping_Rain_Water.java
@@ -23,3 +23,44 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int trap(int[] height) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int ans = 0;
+        for (int i = 0; i < height.length; i ++) {
+            while (!stack.isEmpty() && height[i] >= height[stack.peek()]) {
+                int prev = stack.pop();
+                if (!stack.isEmpty()) {
+                    int tmp = Math.min(height[i] - height[prev], 
+                                        height[stack.peek()] - height[prev]);
+                    ans += (i - stack.peek() - 1) * tmp;
+                }
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+}
+
+class Solution {
+    public int trap(int[] height) {
+        int ans = 0;
+        int l = 0, r = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (l < r) {
+            if (height[l] < height[r]) {
+                if (height[l] > leftMax)
+                    leftMax = height[l];
+                else ans += leftMax - height[l];
+                l ++;
+            } else {
+                if (height[r] > rightMax)
+                    rightMax = height[r];
+                else ans += rightMax - height[r];
+                r --;
+            }
+        }
+        return ans;
+    }
+}
