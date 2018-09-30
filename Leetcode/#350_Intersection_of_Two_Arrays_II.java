f52a8deb89889cca7854da26fe775acbfefa988d
mintycc <mintyck@gmail.com>
2018-09-30 19:37:50 -0400

    Solution to problem 350 updated

--- Leetcode/#350_Intersection_of_Two_Arrays_II.java
+++ Leetcode/#350_Intersection_of_Two_Arrays_II.java
@@ -18,3 +18,25 @@ class Solution {
         return ans;       
     }
 }
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer> ans = new ArrayList<Integer>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) i ++;
+            else if (nums1[i] > nums2[j]) j ++;
+            else {
+                //if (ans.isEmpty() || nums1[i] != ans.get(ans.size() - 1))
+                ans.add(nums1[i]);
+                i ++; j ++;
+            }
+        }
+        int[] rtn = new int[ans.size()];
+        for (i = 0; i < ans.size(); i ++)
+            rtn[i] = ans.get(i);
+        return rtn;
+    }
+}
