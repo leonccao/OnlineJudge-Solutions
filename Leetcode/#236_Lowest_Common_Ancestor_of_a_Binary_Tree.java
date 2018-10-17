caaae659fcf21fa4efabcd64b08aff56e5f3fdfb
mintycc <mintyck@gmail.com>
2018-10-17 00:30:42 -0400

    Solution to problem 236 updated

--- Leetcode/#236_Lowest_Common_Ancestor_of_a_Binary_Tree.java
+++ Leetcode/#236_Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -64,3 +64,90 @@ class Solution {
         return left == null ? right : left;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode tl = lowestCommonAncestor(root.left,  p, q);
+        TreeNode tr = lowestCommonAncestor(root.right, p, q);
+        return tl == null ? tr : (tr == null ? tl : root);
+    }
+}
+
+// One TreeNode maybe not in the tree
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    TreeNode single = new TreeNode(Integer.MAX_VALUE);
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode rtn = lca(root, p, q);
+        if (rtn == null || rtn == single) return null;
+        return rtn;
+    }
+    
+    public TreeNode lca(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        TreeNode tl = lca(root.left,  p, q);
+        TreeNode tr = lca(root.right, p, q);
+        if (root == p || root == q) {
+            if (tl != null || tr != null) return root;
+            return single;
+        }
+        return tl == null ? tr : (tr == null ? tl : root);
+    }
+}
+
+// Add a parent pointer
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    Map<TreeNode, TreeNode> father = new HashMap<TreeNode, TreeNode>();
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        traverse(root);
+        Set<TreeNode> visited = new HashSet<TreeNode>();
+        for (TreeNode tmp = p; tmp != null; tmp = father.getOrDefault(tmp, null)) 
+            visited.add(tmp);
+        for (TreeNode tmp = q; tmp != null; tmp = father.getOrDefault(tmp, null)) 
+            if (visited.contains(tmp)) return tmp;
+        return null;
+    }
+    
+    private void traverse(TreeNode root) {
+        if (root.left != null) {
+            father.put(root.left, root);
+            traverse(root.left);
+        }
+        if (root.right != null) {
+            father.put(root.right, root);
+            traverse(root.right);
+        }
+    }
+}
