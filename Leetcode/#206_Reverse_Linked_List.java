1dd4a1997bff1ccacb6ec330a41ea5b97d1fe475
mintycc <mintyck@gmail.com>
2018-10-20 00:50:40 -0400

    Solution to problem 206 updated

--- Leetcode/#206_Reverse_Linked_List.java
+++ Leetcode/#206_Reverse_Linked_List.java
@@ -23,3 +23,45 @@ class Solution {
         return last;        
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverseIter(head);
+    }
+    
+    ListNode pre;
+    
+    private ListNode reverseRecur(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        pre = dummy;
+        reverseHelper(head);
+        pre.next = null;
+        return dummy.next;
+    }
+    
+    private void reverseHelper(ListNode node) {
+        if (node == null) return;
+        reverseHelper(node.next);
+        pre.next = node;
+        pre = node;
+    }
+    
+    private ListNode reverseIter(ListNode head) {
+        ListNode prev = null, next = null;
+        
+        for (ListNode cur = head; cur != null; cur = next) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+        }
+        return prev;
+    }
+}
