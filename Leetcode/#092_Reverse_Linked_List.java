9902e8637cf860c205d4738060fc25c7e321f01f
mintycc <mintyck@gmail.com>
2018-10-21 17:37:06 -0400

    Solution to problem 092 updated

--- Leetcode/#092_Reverse_Linked_List.java
+++ Leetcode/#092_Reverse_Linked_List.java
@@ -33,3 +33,48 @@ class Solution {
         return headNode.next;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        return reverseIter(head, m, n);
+    }
+    
+    private ListNode reverseRecur(ListNode head, int m, int n) {
+        if (m == n) return head;
+        else if (m > 1) {
+            head.next = reverseRecur(head.next, m - 1, n - 1);
+            return head;
+        } else {
+            ListNode newHead = reverseRecur(head.next, m, n - 1);
+            ListNode headNext = head.next;
+            head.next = head.next.next;
+            headNext.next = head;
+            return newHead;
+        }
+    }
+    
+    private ListNode reverseIter(ListNode head, int m, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode rec = dummy;
+        for (int i = 1; i < m; i ++) rec = rec.next;
+        ListNode cur, prev = rec.next, next = rec.next.next;
+        for (int i = m; i < n; i ++) {
+            cur = next;
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+        }
+        rec.next.next = next;
+        rec.next = prev;
+        return dummy.next;
+    }
+}
