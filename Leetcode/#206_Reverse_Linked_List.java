85fb1ca0d741104b147924712972462a55c36c2c
mintycc <mintyck@gmail.com>
2018-10-21 17:11:50 -0400

    Solution to problem 206 updated

--- Leetcode/#206_Reverse_Linked_List.java
+++ Leetcode/#206_Reverse_Linked_List.java
@@ -65,3 +65,22 @@ class Solution {
         return prev;
     }
 }
+
+// Another recursion solution
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
