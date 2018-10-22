ae59f5cde7b73c2b25dc805585cbadadee5af975
mintycc <mintyck@gmail.com>
2018-10-21 20:03:04 -0400

    Solution to problem 067 updated

--- Leetcode/#067_Add_Binary.java
+++ Leetcode/#067_Add_Binary.java
@@ -13,3 +13,22 @@ class Solution {
         return sb.reverse().toString();
     }
 }
+
+class Solution {
+    public String addBinary(String a, String b) {
+        int base = 2, carry = 0;
+        int len = Math.max(a.length(), b.length());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i ++) {
+            int tmp = carry;
+            if (i < a.length())
+                tmp += a.charAt(a.length() - i - 1) - '0';
+            if (i < b.length())
+                tmp += b.charAt(b.length() - i - 1) - '0';
+            carry = tmp / base;
+            sb.append(tmp % base);
+        }
+        if (carry > 0) sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
