8e207f587d1dacc1586fbafdfb80c362bd5dac80
mintycc <mintyck@gmail.com>
2018-10-22 23:10:46 -0400

    Solution to problem 791 updated

--- Leetcode/791_Custom_Sort_String.java
+++ Leetcode/791_Custom_Sort_String.java
@@ -58,3 +58,22 @@ class Solution {
         return ans.toString();
     }
 }
+
+class Solution {
+    public String customSortString(String S, String T) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : T.toCharArray())
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        StringBuilder sb = new StringBuilder();
+        for (char ch : S.toCharArray()) 
+            if (map.containsKey(ch)) {
+                for (int i = 0; i < map.get(ch); i ++)
+                    sb.append(ch);
+                map.remove(ch);
+            }
+        for (char ch : map.keySet())
+            for (int i = 0; i < map.get(ch); i ++)
+                sb.append(ch);
+        return sb.toString();
+    }
+}
