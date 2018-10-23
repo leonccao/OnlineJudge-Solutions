f2ee7df2a2e627913ec2caea60942d51b5a7395e
mintycc <mintyck@gmail.com>
2018-10-22 22:42:53 -0400

    Solution to problem 138 updated

--- Leetcode/138_Copy_List_with_Random_Pointer.java
+++ Leetcode/138_Copy_List_with_Random_Pointer.java
@@ -26,3 +26,48 @@ public class Solution {
         return dupHead;
     }
 }
+
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null) return null;
+        return copyOneSpace(head);
+    }
+    
+    private RandomListNode copyOneSpace(RandomListNode head) {
+        for (RandomListNode tmp = head; tmp != null; tmp = tmp.next.next) {
+            RandomListNode cur = new RandomListNode(tmp.label);
+            cur.next = tmp.next;
+            tmp.next = cur;
+        }
+        
+        for (RandomListNode tmp = head; tmp != null; tmp = tmp.next.next)
+            tmp.next.random = tmp.random == null ? null : tmp.random.next;
+        
+        RandomListNode copyHead = head.next;
+        for (RandomListNode tmp = head; tmp != null; tmp = tmp.next) {
+            RandomListNode cur = tmp.next;
+            tmp.next = tmp.next.next;
+            cur.next = tmp.next == null ? null : tmp.next.next;
+        }
+        return copyHead;
+    }
+    
+    private RandomListNode copyNSpace(RandomListNode head) {
+        Map<RandomListNode, RandomListNode> map = new HashMap<>();
+        for (RandomListNode tmp = head; tmp != null; tmp = tmp.next)
+            map.put(tmp, new RandomListNode(tmp.label));
+        for (RandomListNode tmp = head; tmp != null; tmp = tmp.next) {
+            map.get(tmp).next   = map.get(tmp.next  );
+            map.get(tmp).random = map.get(tmp.random);
+        }
+        return map.get(head);
+    }
+}
