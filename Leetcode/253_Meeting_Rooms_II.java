6a3aba39141897560fea975ac9bac73af4086cf5
mintycc <mintyck@gmail.com>
2018-10-25 15:38:43 -0400

    Solution to problem 253 updated

--- Leetcode/253_Meeting_Rooms_II.java
+++ Leetcode/253_Meeting_Rooms_II.java
@@ -1,12 +1,4 @@
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
+// old Priority Queue
 class Solution {
     public int minMeetingRooms(Interval[] intervals) {
         Arrays.sort(intervals, new Comparator<Interval>() {
@@ -26,15 +18,7 @@ class Solution {
     }
 }
 
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
+// old & not preferable
 class Solution {
     public int minMeetingRooms(Interval[] intervals) {
         int[] starts = new int[intervals.length];
@@ -51,5 +35,96 @@ class Solution {
             else j ++;
         }
         return ans;
+    }
+}
+
+// Scan line O(nlogn)
+class Solution {
+    class Meeting {
+        int index, val;
+        Meeting(int index, int val) {
+            this.index = index;
+            this.val = val;
+        }
+    }
+    public int minMeetingRooms(Interval[] intervals) {
+        Meeting[] meets = new Meeting[intervals.length * 2];
+        int count = 0;
+        for (Interval interval : intervals) {
+            meets[count ++] = new Meeting(interval.start,  1);
+            meets[count ++] = new Meeting(interval.end,   -1);
+        }
+        Arrays.sort(meets, new Comparator<Meeting>(){
+            public int compare(Meeting a, Meeting b) {
+                return a.index - b.index;
+            }
+        });
+        
+        int sum = 0, ans = 0;
+        for (int i = 0; i < meets.length; i ++) {
+            sum += meets[i].val;
+            while (i + 1 < meets.length && meets[i + 1].index == meets[i].index)
+                sum += meets[++ i].val;
+            ans = Math.max(ans, sum);
+        }
+        return ans;
+    }
+}
+
+// PriorityQueue + Assign Room + In-room Merge Intervals
+class Solution {
+    class Room {
+        int name, end;
+        Room(int name, int end) {
+            this.name = name;
+            this.end = end;
+        }
+    }
+    
+    public int minMeetingRooms(Interval[] intervals) {
+        Arrays.sort(intervals, new Comparator<Interval>(){
+            public int compare(Interval a, Interval b) {
+                return a.start - b.start;
+            }
+        });
+        PriorityQueue<Room> pq = new PriorityQueue<>(new Comparator<Room>(){
+            public int compare(Room a, Room b) {
+                return a.end - b.end;
+            }
+        });
+        List<List<Interval>> rec = new ArrayList<>();
+        for (Interval interval : intervals) {
+            Room cur = null;
+            if (pq.isEmpty() || pq.peek().end > interval.start) {
+                cur = new Room(pq.size(), 0);
+                rec.add(new ArrayList<>());
+            } else cur = pq.poll();
+            cur.end = interval.end;
+            rec.get(cur.name).add(interval);
+            pq.add(cur);
+        }
+        mergeInterval(rec);
+        return pq.size();
+    }
+    
+    private void mergeInterval(List<List<Interval>> rec) {
+        for (int i = 0; i < rec.size(); i ++) {
+            if (rec.get(i).isEmpty()) continue;
+            List<Interval> tmp = new ArrayList<>();
+            Interval last = rec.get(i).get(0);
+            for (int j = 1; j < rec.get(i).size(); j ++)
+                if (last.end < rec.get(i).get(j).start) {
+                    tmp.add(last);
+                    last = rec.get(i).get(j);
+                } else last.end = rec.get(i).get(j).end;
+            tmp.add(last);
+            rec.set(i, tmp);
+        }
+        for (List<Interval> room : rec) {
+            System.out.print("[");
+            for (Interval interval : room)
+                System.out.print("[" + interval.start + ", " + interval.end + "], ");
+            System.out.println("]");
+        }
     }
 }
