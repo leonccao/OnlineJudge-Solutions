79c133c819daad1cb2fd85b0f69d749ee19a8648
mintycc <mintyck@gmail.com>
2018-10-25 08:31:54 -0400

    Solution to problem 238 updated

--- Leetcode/238_Product_of_Array_Except_Self.java
+++ Leetcode/238_Product_of_Array_Except_Self.java
@@ -15,3 +15,37 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int sum = 1;
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i ++) {
+            ans[i] = sum;
+            sum *= nums[i];
+        }
+        sum = 1;
+        for (int i = nums.length - 1;i >= 0; i --) {
+            ans[i] *= sum;
+            sum *= nums[i];
+        }
+        return ans;
+    }
+}
+
+// Use division and zero judge
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int sum = 1, count = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            if (nums[i] == 0) count ++;
+            else sum *= nums[i];
+        }
+        for (int i = 0; i < nums.length; i ++) {
+            nums[i] = nums[i] == 0 ?
+                (count > 1 ? 0 : sum) :
+                (count > 0 ? 0 : sum / nums[i]);
+        }
+        return nums;
+    }
+}
