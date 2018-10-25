93d02280f62f37b2258d8ae3650b5c9c7eeb81de
mintycc <mintyck@gmail.com>
2018-10-25 09:37:02 -0400

    Solution to problem 015 updated

--- Leetcode/015_3Sum.java
+++ Leetcode/015_3Sum.java
@@ -22,3 +22,49 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i ++) {
+            int j = i + 1, k = nums.length - 1;
+            int target = -nums[i], last = Integer.MIN_VALUE;
+            while (j < k) {
+                while (j < k && nums[j] == last) j ++;
+                last = nums[j];
+                while (j < k && nums[j] + nums[k] > target) k --;
+                if (j < k && nums[j] + nums[k] == target)
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+            }
+            while (i + 1 < nums.length && nums[i + 1] == nums[i]) i ++;
+        }
+        return ans;
+    }
+}
+
+// O(n ^ 2) HashMap
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i ++)
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        Set<List<Integer>> ans = new HashSet<>();
+        for (int i = 0; i < nums.length; i ++) {
+            map.put(nums[i], map.get(nums[i]) - 1);
+            if (map.get(nums[i]) == 0)
+                map.remove(nums[i]);
+            for (int j = 0; j < i; j ++) {
+                int tar = - nums[i] - nums[j];
+                if (map.containsKey(tar)) {
+                    List<Integer> tmp = Arrays.asList(nums[j], nums[i], tar);
+                    Collections.sort(tmp);
+                    ans.add(tmp);
+                }
+            }
+        }
+        List<List<Integer>> rtn = new ArrayList<>();
+        rtn.addAll(ans);
+        return rtn;
+    }
+}
