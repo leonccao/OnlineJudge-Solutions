ec942db7cb57b4b0e34a7d4688e913f4899e1994
mintycc <mintyck@gmail.com>
2018-10-26 18:43:30 -0400

    Solution to problem 010 updated

--- Leetcode/010_Regular_Expression_Matching.java
+++ Leetcode/010_Regular_Expression_Matching.java
@@ -30,3 +30,17 @@ class Solution {
         return f[s.length() - 1][p.length() - 1];
     }
 }
+
+// DFS
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (p == null || p.length() == 0)
+            return (s == null || s.length() == 0);
+        if (p.length() == 1 || p.charAt(1) != '*')
+            return (s.length() > 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') &&
+                isMatch(s.substring(1), p.substring(1)));
+        return (isMatch(s, p.substring(2)) || 
+                (s.length() > 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') && isMatch(s.substring(1), p)));
+    }
+}
