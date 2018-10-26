ef9cecd3cd687898144bf9d6ee21a477dd2012e2
mintycc <mintyck@gmail.com>
2018-10-25 22:23:02 -0400

    Solution to problem 088 updated

--- Leetcode/088_Merge_Sorted_Array.java
+++ Leetcode/088_Merge_Sorted_Array.java
@@ -7,3 +7,13 @@ class Solution {
         return;
     }
 }
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = nums1.length - 1; i >= 0; i --) {
+            if (m == 0 || (n > 0 && nums2[n - 1] > nums1[m - 1])) 
+                nums1[i] = nums2[-- n];
+            else nums1[i] = nums1[-- m];
+        }
+    }
+}
