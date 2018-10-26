5f544bff460b9bfc3580285eaa9ef054f6cd6b06
mintycc <mintyck@gmail.com>
2018-10-25 22:11:12 -0400

    Solution to problem 023 updated

--- Leetcode/023_Merge_k_Sorted_Lists.java
+++ Leetcode/023_Merge_k_Sorted_Lists.java
@@ -31,3 +31,24 @@ class Solution {
         return head.next;
     }
 }
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>(){
+            public int compare(ListNode a, ListNode b) {
+                return a.val - b.val;
+            }
+        });
+        for (ListNode list : lists) 
+            if (list != null) pq.add(list);
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while (!pq.isEmpty()) {
+            tail.next = pq.poll();
+            tail = tail.next;
+            if (tail.next != null)
+                pq.add(tail.next);
+        }
+        return head.next;
+    }
+}
