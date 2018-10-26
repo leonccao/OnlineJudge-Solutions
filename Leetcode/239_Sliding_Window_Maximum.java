433e6dfa8aa8e1192b3837acaa45f7ac9e3899af
mintycc <mintyck@gmail.com>
2018-10-26 18:00:37 -0400

    Solution to problem 239 updated

--- Leetcode/239_Sliding_Window_Maximum.java
+++ Leetcode/239_Sliding_Window_Maximum.java
@@ -48,3 +48,27 @@ class Solution {
         return ans;
     }
 }
+
+// Sliding windows start with 0 and end with 0
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> deque = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length + k - 1; i ++) {
+            if (i < nums.length) {
+                while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i])
+                    deque.pollLast();
+                deque.addLast(i);
+            }
+            while (!deque.isEmpty() && i - deque.peekFirst() >= k)
+                deque.pollFirst();
+            ans.add(nums[deque.peekFirst()]);
+        }
+        
+        int[] rtn = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i ++)
+            rtn[i] = ans.get(i);
+        return rtn;
+    }
+}
