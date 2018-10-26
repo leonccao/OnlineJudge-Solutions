ee20deeedc48f83c743d7be7acccadc15677bd74
mintycc <mintyck@gmail.com>
2018-10-26 10:40:24 -0400

    Solution to problem 341 updated

--- Leetcode/341_Flatten_Nested_List_Iterator.java
+++ Leetcode/341_Flatten_Nested_List_Iterator.java
@@ -1,20 +1,4 @@
-/**
- * // This is the interface that allows for creating nested lists.
- * // You should not implement it, or speculate about its implementation
- * public interface NestedInteger {
- *
- *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
- *     public boolean isInteger();
- *
- *     // @return the single integer that this NestedInteger holds, if it holds a single integer
- *     // Return null if this NestedInteger holds a nested list
- *     public Integer getInteger();
- *
- *     // @return the nested list that this NestedInteger holds, if it holds a nested list
- *     // Return null if this NestedInteger holds a single integer
- *     public List<NestedInteger> getList();
- * }
- */
+
 public class NestedIterator implements Iterator<Integer> {
     
     Stack<NestedInteger> stack;
@@ -48,29 +32,6 @@ public class NestedIterator implements Iterator<Integer> {
     }
 }
 
-/**
- * Your NestedIterator object will be instantiated and called as such:
- * NestedIterator i = new NestedIterator(nestedList);
- * while (i.hasNext()) v[f()] = i.next();
- */
-
- /**
- * // This is the interface that allows for creating nested lists.
- * // You should not implement it, or speculate about its implementation
- * public interface NestedInteger {
- *
- *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
- *     public boolean isInteger();
- *
- *     // @return the single integer that this NestedInteger holds, if it holds a single integer
- *     // Return null if this NestedInteger holds a nested list
- *     public Integer getInteger();
- *
- *     // @return the nested list that this NestedInteger holds, if it holds a nested list
- *     // Return null if this NestedInteger holds a single integer
- *     public List<NestedInteger> getList();
- * }
- */
 public class NestedIterator implements Iterator<Integer> {
 
     Stack<NestedInteger> stack;
@@ -98,8 +59,29 @@ public class NestedIterator implements Iterator<Integer> {
     }
 }
 
-/**
- * Your NestedIterator object will be instantiated and called as such:
- * NestedIterator i = new NestedIterator(nestedList);
- * while (i.hasNext()) v[f()] = i.next();
- */
+// new 
+public class NestedIterator implements Iterator<Integer> {
+
+    Stack<NestedInteger> stack;
+    
+    public NestedIterator(List<NestedInteger> nestedList) {
+        stack = new Stack<>();
+        for (int i = nestedList.size() - 1; i >= 0; i --)
+            stack.push(nestedList.get(i));
+    }
+
+    @Override
+    public Integer next() {
+        return stack.pop().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty() && !stack.peek().isInteger()) {
+            List<NestedInteger> cur = stack.pop().getList();
+            for (int i = cur.size() - 1; i >= 0; i --)
+                stack.push(cur.get(i));
+        }
+        return !stack.isEmpty();
+    }
+}
