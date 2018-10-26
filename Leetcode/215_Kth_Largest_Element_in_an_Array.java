df6f17913084308ccd79d83466349c6ec070d09c
mintycc <mintyck@gmail.com>
2018-10-26 16:20:01 -0400

    Solution to problem 215 updated

--- Leetcode/215_Kth_Largest_Element_in_an_Array.java
+++ Leetcode/215_Kth_Largest_Element_in_an_Array.java
@@ -26,3 +26,32 @@ class Solution {
         return findkth(nums, 0, nums.length - 1, k - 1);
     }
 }
+
+
+// Quick selection new
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, k - 1);
+    }
+    
+    private int quickSelect(int[] nums, int l, int r, int k) {
+        if (l == r) return nums[l];
+        int pivot = nums[r];
+        int i = l, j = r - 1;
+        do {
+            while (i < r && nums[i] > pivot) i ++;
+            while (l < j && nums[j] < pivot) j --;
+            if (i < j) swap(nums, i ++, j --);
+        } while (i < j);
+        swap(nums, i, r);
+        if (i == k) return nums[i];
+        if (k < i) return quickSelect(nums, l, i - 1, k);
+        else return quickSelect(nums, i + 1, r, k);
+    }
+    
+    private void swap(int[] nums, int a, int b) {
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
+}
