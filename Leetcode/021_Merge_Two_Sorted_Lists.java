f1de76f10438e3c897f8b4bac3e947793771998c
mintycc <mintyck@gmail.com>
2018-10-25 22:01:15 -0400

    Solution to problem 021 updated

---
+++ Leetcode/021_Merge_Two_Sorted_Lists.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while (l1 != null || l2 != null) {
+            if (l1 == null || (l2 != null && l2.val < l1.val)) {
+                tail.next = l2;
+                l2 = l2.next;
+            } else {
+                tail.next = l1;
+                l1 = l1.next;
+            }
+            tail = tail.next;
+        }
+        return head.next;
+    }
+}
