2bd70819f616833fd5d1a326fa7f8c884894db57
mintycc <mintyck@gmail.com>
2018-10-26 23:39:31 -0400

    Solution to problem 173 updated

--- Leetcode/173_Binary_Search_Tree_Iterator.java
+++ Leetcode/173_Binary_Search_Tree_Iterator.java
@@ -32,8 +32,33 @@ public class BSTIterator {
     }
 }
 
-/**
- * Your BSTIterator will be called like this:
- * BSTIterator i = new BSTIterator(root);
- * while (i.hasNext()) v[f()] = i.next();
- */
+// new
+public class BSTIterator {
+
+    Stack<TreeNode> stack;
+    
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        if (root != null) {
+            stack.push(root);
+            while (stack.peek().left != null)
+                stack.push(stack.peek().left);
+        }
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode cur = stack.pop();
+        if (cur.right != null) {
+            stack.push(cur.right);
+            while (stack.peek().left != null)
+                stack.push(stack.peek().left);
+        }
+        return cur.val;
+    }
+}
