53f5a553be1e58ec5670bc43fa313f226606b5a5
mintycc <mintyck@gmail.com>
2018-10-27 18:17:06 -0400

    Solution to problem 680 updated

--- Leetcode/680_Valid_Palindrome.java
+++ Leetcode/680_Valid_Palindrome.java
@@ -24,3 +24,23 @@ class Solution {
         return false;
     }
 }
+
+// new 
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r && s.charAt(l) == s.charAt(r)) {
+            l ++; r --;
+        }
+        if (l < r) return (isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1));
+        return true;
+    }
+    
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            l ++; r --;
+        }
+        return true;
+    }
+}
