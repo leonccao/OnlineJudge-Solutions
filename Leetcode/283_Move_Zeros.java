fe6c6878dc2ad984106a6506d28c244c8effe911
mintycc <mintyck@gmail.com>
2018-10-28 00:10:37 -0400

    Solution to problem 283 updated

--- Leetcode/283_Move_Zeros.java
+++ Leetcode/283_Move_Zeros.java
@@ -8,3 +8,17 @@ class Solution {
             nums[now ++] = 0;
     }
 }
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i ++)
+            if (nums[i] != 0) {
+                if (i == j) j ++;
+                else {
+                    nums[j ++] = nums[i];
+                    nums[i] = 0;
+                }
+            }
+    }
+}
