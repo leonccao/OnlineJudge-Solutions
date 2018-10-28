69f46310ae80462c2bfa69ed29524c9c92b8689f
mintycc <mintyck@gmail.com>
2018-10-28 19:27:26 -0400

    Solution to problem 124 updated

--- Leetcode/124_Binary_Tree_Maximum_Path_Sum.java
+++ Leetcode/124_Binary_Tree_Maximum_Path_Sum.java
@@ -26,3 +26,22 @@ class Solution {
         return maxn;
     }
 }
+
+// new 
+class Solution {
+    
+    int max = Integer.MIN_VALUE;
+    
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return max;
+    }
+    
+    private int helper(TreeNode root) {
+        if (root == null) return 0;
+        int l = Math.max(helper(root.left),  0);
+        int r = Math.max(helper(root.right), 0);
+        max = Math.max(max, l + r + root.val);
+        return Math.max(l + root.val, r + root.val);
+    }
+}
