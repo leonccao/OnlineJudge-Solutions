0378e8c6583d09fd02f121f199994b36958edc0b
mintycc <mintyck@gmail.com>
2018-10-28 15:36:46 -0400

    Solution to problem 042 updated

--- Leetcode/042_Trapping_Rain_Water.java
+++ Leetcode/042_Trapping_Rain_Water.java
@@ -64,3 +64,54 @@ class Solution {
         return ans;
     }
 }
+
+// new stack
+class Solution {
+    public int trap(int[] height) {
+        Stack<Integer> stack = new Stack<>();
+        int ans = 0;
+        for (int i = 0; i < height.length; i ++) {
+            while (!stack.isEmpty() && height[i] >= height[stack.peek()]) {
+                int cur = height[stack.pop()];
+                if (!stack.isEmpty())
+                    ans += (i - stack.peek() - 1) * (Math.min(height[i], height[stack.peek()]) - cur);
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+}
+
+// new two scan
+class Solution {
+    public int trap(int[] height) {
+        int[] water = new int[height.length];
+        int bar = 0, ans = 0;
+        for (int i = 0; i < height.length; i ++) {
+            bar = Math.max(bar, height[i]);
+            water[i] = bar;
+        }
+        bar = 0;
+        for (int i = height.length - 1; i >= 0; i --) {
+            bar = Math.max(bar, height[i]);
+            water[i] = Math.min(water[i], bar);
+            ans += water[i] - height[i];
+        }
+        return ans;
+    }
+}
+
+// one scan two pointer
+class Solution {
+    public int trap(int[] height) {
+        int ans = 0, l = 0, r = height.length - 1;
+        int barL = 0, barR = 0;
+        while (l <= r) {
+            barL = Math.max(barL, height[l]);
+            barR = Math.max(barR, height[r]);
+            if (barL < barR) ans += barL - height[l ++];
+            else ans += barR - height[r --];
+        }
+        return ans;
+    }
+}
