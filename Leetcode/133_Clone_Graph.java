8b12d89b56338c39ae8143e62de365efe997228f
mintycc <mintyck@gmail.com>
2018-10-28 12:27:05 -0400

    Solution to problem 133 updated

--- Leetcode/133_Clone_Graph.java
+++ Leetcode/133_Clone_Graph.java
@@ -31,3 +31,41 @@ public class Solution {
         return map.get(node);
     }
 }
+
+// new BFS
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) return null;
+        Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+        map.put(node, new UndirectedGraphNode(node.label));
+        Queue<UndirectedGraphNode> queue = new LinkedList<>();
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            UndirectedGraphNode cur = queue.poll();
+            List<UndirectedGraphNode> list = cur.neighbors;
+            for (UndirectedGraphNode nb : list) {
+                if (!map.containsKey(nb)) {
+                    queue.add(nb);
+                    map.put(nb, new UndirectedGraphNode(nb.label));
+                }
+                map.get(cur).neighbors.add(map.get(nb));
+            }
+        }
+        return map.get(node);
+    }
+}
+
+// new DFS
+public class Solution {
+    Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+    
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) return null;
+        if (map.containsKey(node)) return map.get(node);
+        map.put(node, new UndirectedGraphNode(node.label));
+        List<UndirectedGraphNode> list = node.neighbors;
+        for (UndirectedGraphNode nb : list)
+            map.get(node).neighbors.add(cloneGraph(nb));
+        return map.get(node);
+    }
+}
