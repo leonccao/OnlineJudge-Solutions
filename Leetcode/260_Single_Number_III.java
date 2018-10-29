008090b430b866783f7929208d4d3758edee42e5
mintycc <mintyck@gmail.com>
2018-10-28 23:28:19 -0400

    Solution to problem 260 updated

--- Leetcode/260_Single_Number_III.java
+++ Leetcode/260_Single_Number_III.java
@@ -12,3 +12,17 @@ class Solution {
         return ans;
     }
 }
+
+// new
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int x = 0, a = 0, b = 0;
+        for (int num : nums) x ^= num;
+        x = Integer.lowestOneBit(x);
+        for (int num : nums)
+            if ((x & num) == 0) a ^= num;
+            else b ^= num;
+        return new int[]{a, b};
+    }
+}
