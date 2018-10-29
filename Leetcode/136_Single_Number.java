73a9b54b72bcbb273a6dc3528ddd460c89d7cd26
mintycc <mintyck@gmail.com>
2018-10-28 22:51:07 -0400

    Solution to problem 1336 updated

--- Leetcode/136_Single_Number.java
+++ Leetcode/136_Single_Number.java
@@ -6,3 +6,13 @@ class Solution {
         return result;
     }
 }
+
+// new 
+class Solution {
+    public int singleNumber(int[] nums) {
+        int x = 0;
+        for (int num : nums)
+            x ^= num;
+        return x;
+    }
+}
