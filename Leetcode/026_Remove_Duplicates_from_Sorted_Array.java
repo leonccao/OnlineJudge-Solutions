1375c7bde99dee5293cd95d356d903393ecbeae3
mintycc <mintyck@gmail.com>
2018-10-29 14:12:13 -0400

    Solution to problem 026 updated

---
+++ Leetcode/026_Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int j = 0;
+        for (int i = 1; i < nums.length; i ++)
+            if (nums[i] != nums[j])
+                nums[++ j] = nums[i];
+        return j + 1;
+    }
+}
