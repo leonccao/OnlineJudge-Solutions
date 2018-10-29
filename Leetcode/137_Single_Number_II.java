7516390fd48f013578bf0ba896bfff0a1d3e7187
mintycc <mintyck@gmail.com>
2018-10-28 23:17:23 -0400

    Solution to problem 137 updated

--- Leetcode/137_Single_Number_II.java
+++ Leetcode/137_Single_Number_II.java
@@ -9,3 +9,16 @@ class Solution {
         return b;
     }
 }
+
+// new 
+class Solution {
+    public int singleNumber(int[] nums) {
+        int a = 0, b = 0;
+        for (int c : nums) {
+            int tmp = (~a & b & c) | (a & ~b & ~c);
+            b = (~a & ~b & c) | (~a & b & ~c);
+            a = tmp;
+        }
+        return b;
+    }
+}
