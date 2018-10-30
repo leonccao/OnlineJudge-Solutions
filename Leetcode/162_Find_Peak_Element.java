39ea810e1f3323b536bf8246c0c380148d24919f
mintycc <mintyck@gmail.com>
2018-10-30 00:29:19 -0400

    Solution to problem 162 updated

--- Leetcode/162_Find_Peak_Element.java
+++ Leetcode/162_Find_Peak_Element.java
@@ -12,3 +12,19 @@ class Solution {
         return l;
     }
 }
+
+// new 
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums.length == 0) return -1;
+        
+        int l = 0, r = nums.length;
+        while (l < r - 1) {
+            int mid = l + (r - l) / 2 - 1;
+            if (nums[mid] < nums[mid + 1])
+                l = mid + 1;
+            else r = mid + 1;
+        }
+        return l;
+    }
+}
