b647127764fdc8503d966a5f90b0299c7acf56a3
mintycc <mintyck@gmail.com>
2018-10-31 16:25:08 -0400

    Solution to problem 904 updated

--- Leetcode/904_Fruit_Into_Baskets.java
+++ Leetcode/904_Fruit_Into_Baskets.java
@@ -26,3 +26,36 @@ class Solution {
         return ans;
     }
 }
+
+// new 
+class Solution {
+    public int totalFruit(int[] tree) {
+        int type1, type2, last1, last2, hold1, hold2, ans;
+        type1 = type2 = last1 = last2 = -1;
+        hold1 = hold2 = ans = 0;
+        
+        for (int i = 0; i < tree.length; i ++) {
+            if (type1 == tree[i] || type2 == tree[i]) {
+                if (type1 == tree[i]) { 
+                    hold1 ++; last1 = i; 
+                } else { 
+                    hold2 ++; last2 = i; 
+                }
+            } else {
+                if (last1 < last2) { 
+                    hold2 = i - 1 - last1;
+                    type1 = tree[i]; 
+                    last1 = i; 
+                    hold1 = 1;  
+                } else { 
+                    hold1 = i - 1 - last2;
+                    type2 = tree[i]; 
+                    last2 = i; 
+                    hold2 = 1; 
+                }
+            }
+            ans = Math.max(ans, hold1 + hold2);
+        }
+        return ans;
+    }
+}
